e96bc56273d376e4117eb7d0ce6251ebb9563ae5
berat-erkul <beraterkul00@gmail.com>
2025-08-24 14:57:13 +0300

    Starting to the Security part

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TicketingProjectSecurityApplication {
+public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketingProjectSecurityApplication.class, args);
+        SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
     @Bean
